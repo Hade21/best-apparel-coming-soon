c8de735fcab0a5925738a167099d917e0c26e9bc
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-01-19 09:07:59 +0800

    add interactive button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// const btn = document.querySelector('#tombol');
-// const submit = document.querySelector('#email');
+const warning = document.querySelector('#error');
+const errorMsg = document.querySelector('.error-message');
 
-// btn.addEventListener('click', function() {
-//     submit.preventDefault();
-//     console.log(submit);
-// })
+function validasi(email) {
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value.match(mailformat)) {
+        return true;
+    } else {
+        errorMsg.innerHTML = 'Please provide a valid email';
+        warning.style.display = 'block';
+        console.log(warning.style.display);
+        return false;
+    }
+}
